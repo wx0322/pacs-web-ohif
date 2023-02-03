54f4db782c8ad796c2190fad8f79a52e8d1aea0b
wang xuan <wx_0322@163.com>
2023-02-03 13:53:46 +0800

    修改WhiteLabeling的PropType类型为object

--- ohif-v3/platform/ui/src/components/Header/Header.tsx
+++ ohif-v3/platform/ui/src/components/Header/Header.tsx
@@ -5,60 +5,58 @@ import classNames from 'classnames';
 import { NavBar, Svg, Icon, IconButton, Dropdown } from '../';
 
 function Header({ children, menuOptions, isReturnEnabled, onClickReturnButton, isSticky, WhiteLabeling }) {
-  const { t } = useTranslation('Header');
+    const { t } = useTranslation('Header');
 
-  // TODO: this should be passed in as a prop instead and the react-router-dom
-  // dependency should be dropped
-  const onClickReturn = () => {
-    if (isReturnEnabled && onClickReturnButton) {
-      onClickReturnButton()
+    // TODO: this should be passed in as a prop instead and the react-router-dom
+    // dependency should be dropped
+    const onClickReturn = () => {
+        if (isReturnEnabled && onClickReturnButton) {
+            onClickReturnButton()
+        }
+    };
+
+    const CustomLogo = (React) => {
+        return WhiteLabeling.createLogoComponentFn(React)
     }
-  };
 
-  const CustomLogo = (React) => {
-    return WhiteLabeling.createLogoComponentFn(React)
-  }
-
-  return (
-    <NavBar className='justify-between border-b-4 border-black' isSticky={isSticky}>
-      <div className="flex justify-between flex-1">
-        <div className="flex items-center">
-          {/* // TODO: Should preserve filter/sort
-              // Either injected service? Or context (like react router's `useLocation`?) */}
-          <div
-            className={classNames("inline-flex items-center mr-3", isReturnEnabled && 'cursor-pointer')}
-            onClick={onClickReturn}
-          >
-            {isReturnEnabled && <Icon name="chevron-left" className="w-8 text-primary-active" />}
-            <div className="ml-4">{WhiteLabeling ? CustomLogo(React) : <Svg name="logo-ohif" />}</div>
-          </div>
-        </div>
-        <div className="flex items-center">{children}</div>
-        <div className="flex items-center">          
-        </div>
-      </div>
-    </NavBar>
-  );
+    return (
+        <NavBar className='justify-between border-b-4 border-black' isSticky={isSticky}>
+            <div className="flex justify-between flex-1">
+                <div className="flex items-center">
+                    <div
+                        className={classNames("inline-flex items-center mr-3", isReturnEnabled && 'cursor-pointer')}
+                        onClick={onClickReturn}
+                    >
+                        {isReturnEnabled && <Icon name="chevron-left" className="w-8 text-primary-active" />}
+                        <div className="ml-4">{WhiteLabeling ? CustomLogo(React) : <Svg name="logo-ohif" />}</div>
+                    </div>
+                </div>
+                <div className="flex items-center">{children}</div>
+                <div className="flex items-center">
+                </div>
+            </div>
+        </NavBar>
+    );
 }
 
 Header.propTypes = {
-  menuOptions: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      icon: PropTypes.string,
-      onClick: PropTypes.func.isRequired,
-    })
-  ),
-  children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
-  isReturnEnabled: PropTypes.bool,
-  isSticky: PropTypes.bool,
-  onClickReturnButton: PropTypes.func,
-  WhiteLabeling: PropTypes.element,
+    menuOptions: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            icon: PropTypes.string,
+            onClick: PropTypes.func.isRequired,
+        })
+    ),
+    children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+    isReturnEnabled: PropTypes.bool,
+    isSticky: PropTypes.bool,
+    onClickReturnButton: PropTypes.func,
+    WhiteLabeling: PropTypes.object,
 };
 
 Header.defaultProps = {
-  isReturnEnabled: true,
-  isSticky: false
+    isReturnEnabled: true,
+    isSticky: false
 };
 
 export default Header;
